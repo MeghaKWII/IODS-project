a7972721493c983687c2eb1c84419d2028287095
unknown <megha.khanduri@helsinki.fi>
2023-11-26 16:56:53 +0530

    data wrangling Assignment 4

---
+++ create_human.R
@@ -0,0 +1,3 @@
+library(readr)
+hd <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/human_development.csv")
+gii <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/gender_inequality.csv", na = "..")
