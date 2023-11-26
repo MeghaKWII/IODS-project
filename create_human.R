025fd0feb0e24a9a95c0d746b2656f1b7f2e9f17
unknown <megha.khanduri@helsinki.fi>
2023-11-26 20:29:01 +0530

    k-means clustering

--- create_human.R
+++ create_human.R
@@ -1,3 +1,11 @@
 library(readr)
 hd <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/human_development.csv")
 gii <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/gender_inequality.csv", na = "..")
+
+dim(hd)
+str(hd)
+summary(hd)
+
+dim(gii)
+str(gii)
+summary(gii)
