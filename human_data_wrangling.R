85134af09ef36f4bff30d64c008431d8b8739d24
unknown <megha.khanduri@helsinki.fi>
2023-12-02 15:48:53 +0530

    Assignmnet faive

--- human_data_wrangling.R
+++ human_data_wrangling.R
@@ -6,3 +6,5 @@ human2 <- droplevels(human2)
 dim(human)
 human2 <- human2[,c("country", "Edu2FM", "Labo.FM", "Edu.Exp", "Life.exp", "GNI", "MMR", "Ado.Birth", "Parli.rep" )]
 dim(human2)
+
+write.csv(human2, file = "human2")
