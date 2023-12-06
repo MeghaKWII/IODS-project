3eba09dca55ee5f23ad4a32b582ae81930c02793
unknown <megha.khanduri@helsinki.fi>
2023-12-06 11:44:44 +0530

    data wrangling

--- data_wrangling_assignment6.R
+++ data_wrangling_assignment6.R
@@ -6,6 +6,7 @@ library(ggplot2)
 library(GGally)
 library(DT)
 library(tibble)
+library(tidyr)
 
 
 
@@ -16,9 +17,29 @@ write.csv(BPRS, file = "BPRS")
 dim(BPRS)
 str(BPRS)
 
-RATS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/rats.txt", sep =" ", header = T, stringsAsFactors = TRUE)
-
+RATS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/rats.txt", header = TRUE, sep = '\t')
 write.csv(RATS, file = "RATS")
 
 dim(RATS)
 str(RATS)
+
+BPRS<- read.csv("BPRS")
+RATS <- read.csv("RATS")
+
+
+BPRS$treatment <- factor(BPRS$treatment)
+BPRS$subject <- factor(BPRS$subject)
+BPRSL <-  pivot_longer(BPRS, cols=-c(treatment,subject),names_to = "weeks",values_to = "bprs") %>% arrange(weeks)
+BPRSL <-  BPRSL %>% mutate(week = as.integer(substr(weeks,5,5)))
+
+glimpse(BPRSL)
+
+glimpse(BPRS)
+
+
+RATS$ID <- factor(RATS$ID)
+RATS$Group <- factor(RATS$Group)
+RATSL <- pivot_longer(RATS, cols=-c(ID,Group), names_to = "WD",values_to = "Weight")  %>%  mutate(Time = as.integer(substr(WD,3,4))) %>% arrange(Time)
+
+glimpse(RATSL)
+
